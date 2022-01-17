188ca79193d5f1c3ca69d5c9a59e3a2e11d899ff
pix666 <pix666@ya.ru>
2022-01-17 15:00:17 +0400

    display examples on home page

--- tests/app/views.py
+++ tests/app/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
 
+from .models import Example
+
 
 class IndexView(TemplateView):
     template_name = 'app/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["items"] = Example.objects.order_by("-id")
+        return context
